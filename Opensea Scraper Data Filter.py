2f236e06f5edf1fd0a5b355d7be75d533d6d4998
Immersified <115187612+Immersified@users.noreply.github.com>
2022-10-24 20:13:06 +0200

    Add files via upload
    Inorder to analyze data created by the opensea scraper, a filter is needed. Which filters out faulty data like nan numbers or '---' strings. Moreover, it filters data based on floor price to find out which collections are viable to bid on. Once that's finished, an excel file is made that will be used in the #Opensea Data Graph where the collections are visualized in graphs.

---
+++ Opensea Scraper Data Filter.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 16 20:17:32 2022
+
+@author: Thomas
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from importlib import reload
+plt=reload(plt)
+
+data = pd.read_excel(r"E:\Thomas\Downloads\Nft_Table 122.xlsx", header= 0, decimal='.')
+
+data_all = pd.DataFrame()
+
+for k in range(122):
+    data = pd.read_excel(r"E:\Thomas\Documents\Investing\Opensea Scraper\Nftfiles\Nft_Table " + str(k) + ".xlsx", header= 0, decimal='.')
+    data_all = pd.concat([data_all, data])
+
+data_all = data_all.dropna()
+data_all.reset_index(drop=True, inplace=True)
+
+best_offer = []
+
+filter_name = [i for i, e in enumerate(data_all['Name']) if e == 'ApeX NFT Predator' or e == 'UNDERWATER CLUB OfficaI']
+data_all = data_all.drop(filter_name)
+data_all.reset_index(drop=True, inplace=True)
+
+for a in data_all['Best offer']:
+    if a == '< 0.01':
+        meme = float(a.replace('<', ''))
+        best_offer.append(meme)
+    else:
+        best_offer.append(a)
+
+data_all['Best offer'] = best_offer
+
+floor_price = []
+
+for b in data_all['Floor']:
+    if b == '< 0.01':
+        meme = float(b.replace('<', ''))
+        floor_price.append(meme)
+    else:
+        floor_price.append(b)
+
+data_all['Floor'] = floor_price
+
+supply = data_all['Supply']
+"Supply column filter"
+filter = [i for i, e in enumerate(supply) if e == '---' or pd.isnull(e) == True or e < 100]
+supply_filter = data_all.drop(filter)
+supply_filter.reset_index(drop=True, inplace=True)
+
+"Listing filter"
+filter1 = [i for i, e in enumerate(supply_filter['Act.Listings']) if e == 0 or pd.isnull(e) == True]
+supply_filter1 = supply_filter.drop(filter1)
+supply_filter1.reset_index(drop=True, inplace=True)
+
+"Best offer filter"
+filter2 = [i for i, e in enumerate(supply_filter1['Best offer']) if e == '---']
+data_filter = supply_filter1.drop(filter2)
+data_filter_nobids = supply_filter1.filter(items = filter2, axis = 0)
+data_filter.reset_index(drop=True, inplace=True)
+
+"Floor price filter"
+filter3 = [i for i, e in enumerate(data_filter['Floor']) if e <= 0.8 or e >= 1.5 or e == '---']
+data_filter_final = data_filter.drop(filter3)
+data_filter_final.reset_index(drop=True, inplace=True)
+
+Royalty = []
+
+for k in data_filter_final['Royalty']:
+    if k < 1:
+        Royalty.append(k*100)
+    else:
+        Royalty.append(k)
+
+data_filter_final['Royalty'] = Royalty
+
+"Quick Maths"
+profit = data_filter_final['Floor']*(100-(data_filter_final['Royalty'] + 2.5))/100-data_filter_final['Best offer']
+data_filter_final['Profit'] = profit
+
+filter4 = [i for i, e in enumerate(data_filter_final['Profit']) if e <= 0]
+data_filter_profit = data_filter_final.drop(filter4)
+data_filter_profit.reset_index(drop=True, inplace=True)
+
+profit_floor = data_filter_profit['Profit']/data_filter_profit['Floor'] * 100
+data_filter_profit['Profit / Floor (%)'] = profit_floor
+sales_list = data_filter_profit['Sales 1D'] / data_filter_profit['Act.Listings'] * 100
+data_filter_profit['Sales / List (%)'] = sales_list
+
+"Best item factor"
+data_filter_profit['Ranking'] = data_filter_profit['Profit / Floor (%)'] * 2 + data_filter_profit['Sales / List (%)']
+
+"Sorting based on Ranking"
+data_filter_profit.sort_values(by=['Ranking'], ascending = False, inplace = True)
+data_filter_profit.reset_index(drop=True, inplace=True)
+
+"Export file"
+data_filter_profit.to_excel('Nft Bidding Rankings.xlsx')
